86516b04463dc56083745d126e5e9634a2d669a6
Tobshub <tobanigeorge@gmail.com>
2024-01-29 00:05:50 +0100

    fix tests

--- tobuilds_test.go
+++ tobuilds_test.go
@@ -12,17 +12,21 @@ import (
 var efs embed.FS
 
 func TestRun(t *testing.T) {
-	c := tobuilds.Init(&efs)
+	ctx := tobuilds.Init(&efs)
 	defer tobuilds.End()
-	assert.NilError(t, c.Run(tobuilds.PlatformLinux, "test/test.sh", "test one"))
-	assert.NilError(t, c.Run(tobuilds.PlatformLinux,
+	assert.NilError(t, ctx.Run(tobuilds.PlatformLinux, "test/test.sh", "test one"))
+	assert.NilError(t, ctx.Run(tobuilds.PlatformLinux,
 		"https://github.com/tobsdb/tobsdb/releases/download/v0.1.2-alpha/tdb-generate_Linux_x86_64", "-schema", "$TABLE a {\nb String\n}"))
-	assert.NilError(t, c.Run(tobuilds.PlatformWindows,
+	assert.NilError(t, ctx.Run(tobuilds.PlatformWindows,
 		"https://github.com/tobsdb/tobsdb/releases/download/v0.1.2-alpha/tdb-generate_Windows_x86_64.exe", "-schema", "$TABLE a {\nb String\n}"))
-	tr := c.NewArchiveTarGz(tobuilds.PlatformLinux, "test/test.tar.gz")
-	assert.NilError(t, tr.Run("test.sh", "test two"))
-	assert.NilError(t, tr.Run("test.sh", "test three"))
-	z, err := c.NewArchiveZip(tobuilds.PlatformLinux, "test/test.zip")
+	tr := ctx.NewArchiveTarGz(tobuilds.PlatformLinux, "test/test.tar.gz")
+	if tr != nil {
+		assert.NilError(t, tr.Run("test.sh", "test two"))
+		assert.NilError(t, tr.Run("test.sh", "test three"))
+	}
+	z, err := ctx.NewArchiveZip(tobuilds.PlatformWindows, "test/test.zip")
 	assert.NilError(t, err)
-	assert.NilError(t, z.Run("test.sh", "test four"))
+	if z != nil {
+		assert.NilError(t, z.Run("test.sh", "test four"))
+	}
 }
